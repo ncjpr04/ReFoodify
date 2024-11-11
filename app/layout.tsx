85b796cfafcbe88243eebe352b7779b74a3be1f2
Nitin <nitinchoudhary22112004@gmail.com>
2024-11-11 08:41:37 +0530

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 // import localFont from "next/font/local";
 import "./globals.css";
 import { AuthProvider } from './context/AuthContext';
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body >
         <AuthProvider>
           {children}
+          <Analytics />
         </AuthProvider>
       </body>
     </html>
